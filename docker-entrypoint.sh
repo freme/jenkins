ac47ddfc3998b5b6f6be2cae3d3b50fce91cb1ef
Thomas William <thomas.william@logmein.com>
2018-06-01 11:28:25 +0200

    cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,11 @@
 set -e
 
 # this if will check if the first argument is a flag
+# and add getFailedStepsLogs
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+    # prefix with getFailedStepsLogs
     set -- getFailedStepsLogs "$@"
-    echo 'detected flag'
 fi
 
-# check for the expected command
-if [ "$1" = 'getFailedStepsLogs' ]; then
-    echo 'command is getFailedStepsLogs'
-    exec getFailedStepsLogs "$@"
-fi
-
-# else default to run whatever the user wanted like "bash" or "sh"
 exec "$@"
 
