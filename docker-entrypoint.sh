3ce690a1cbb7070429253966e6776327a6b74763
Thomas William <thomas.william@logmein.com>
2018-06-01 11:20:29 +0200

    tesing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,12 @@ set -e
 # this if will check if the first argument is a flag
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
     set -- getFailedStepsLogs "$@"
+    echo 'detected flag'
 fi
 
 # check for the expected command
 if [ "$1" = 'getFailedStepsLogs' ]; then
+    echo 'command is getFailedStepsLogs'
     exec getFailedStepsLogs "$@"
 fi
 
